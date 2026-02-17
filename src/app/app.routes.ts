dfc50997c13cf955482af9ba636de0a3c4c4bf31
JordanBRSA <jordan.barbosa-joal@toulouse.miage.fr>
2026-02-17 23:16:44 +0100

    KAN-12 feat: Ajout composant & route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PlaylistList } from './features/playlist-list/playlist-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PlaylistList
+  },
+];
---
+++ src/app/features/playlist-list/playlist-list.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-playlist-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './playlist-list.html',
+  styleUrl: './playlist-list.css',
+})
+export class PlaylistList {
+
+}
